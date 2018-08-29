f35f8b7a63b57e4feb2dbcadbfc2fdc4189699e4
ziggybuilds <ellisjoshuag@gmail.com>
2018-08-29 09:26:51 -0400

    added hero tweens

--- js/script.js
+++ js/script.js
@@ -14,11 +14,11 @@ jQuery(document).ready(function ($) {
 
 		$('#popUpClose').on('click', function (e) {
 			e.preventDefault();
-			if ($popUp.is(":visible") === true) {
+			if ($popUp.is(':visible') === true) {
 				// reloads the iframe to stop the video
 				var iframes = $('iframe');
 				if (iframes != null) {
-					for (var i = 0; i < iframes.length; i++) {
+					for (var i = 0; i < iframes.length; i += 1) {
 						iframes[i].src = iframes[i].src;
 					}
 				}
@@ -30,12 +30,12 @@ jQuery(document).ready(function ($) {
 	// animation controls
 	var controller = new ScrollMagic.Controller();
 	function scrollReveal(elem) {
-		TweenMax.to(elem, 0, { css: { 'opacity': '0' } });
+		TweenMax.to(elem, 0, { css: { opacity: '0' } });
 		new ScrollMagic.Scene({
 			triggerElement: elem,
 			offset: '-50px',
 			reverse: false
-		}).setTween(elem, 1, { css: { 'opacity': '1' } }).addTo(controller);
+		}).setTween(elem, 1, { css: { opacity: '1' } }).addTo(controller);
 	}
 
 	// feed__articles__article
@@ -45,6 +45,12 @@ jQuery(document).ready(function ($) {
 			scrollReveal($articles[i]);
 		}
 	})();
+
+	// header animation styling
+	// header text
+	var $heroTitle = $('.header__titleWrapper__title');
+	var $heroSubTitle = $('.header__titleWrapper__subtitle');
+	var tl = new TimelineMax().set($heroTitle, { x: -50 }).set($heroTitle, { css: { opacity: '0' } }).set($heroSubTitle, { x: -50 }).set($heroSubTitle, { css: { opacity: '0' } }).to($heroTitle, 1, { x: 0 }).to($heroTitle, 2, { css: { opacity: '1' } }, '-=0.8').to($heroSubTitle, 1, { x: 0 }, '-=1').to($heroSubTitle, 2, { css: { opacity: '1' } }, '-=.8');
 });
 
 jQuery(document).ready(function ($) {
--- src/js/action.js
+++ src/js/action.js
@@ -12,13 +12,13 @@ jQuery(document).ready(($) => {
 
 		$('#popUpClose').on('click', (e) => {
 			e.preventDefault();
-			if ($popUp.is(":visible") === true) {
+			if ($popUp.is(':visible') === true) {
 				// reloads the iframe to stop the video
 				const iframes = $('iframe');
 				if (iframes != null) {
-				    for (let i = 0; i < iframes.length; i++) {
-				        iframes[i].src = iframes[i].src;
-				    }
+					for (let i = 0; i < iframes.length; i += 1) {
+						iframes[i].src = iframes[i].src;
+					}
 				}
 				$popUp.fadeOut('fast');
 			}
@@ -28,14 +28,14 @@ jQuery(document).ready(($) => {
 	// animation controls
 	const controller = new ScrollMagic.Controller();
 	function scrollReveal(elem) {
-		TweenMax.to(elem, 0, {css: {'opacity': '0'}});
+		TweenMax.to(elem, 0, { css: { opacity: '0' } });
 		new ScrollMagic.Scene({
 			triggerElement: elem,
 			offset: '-50px',
 			reverse: false,
 		})
-		.setTween(elem, 1, {css: {'opacity': '1'}})
-		.addTo(controller);
+			.setTween(elem, 1, { css: { opacity: '1' } })
+			.addTo(controller);
 	}
 
 	// feed__articles__article
@@ -45,4 +45,18 @@ jQuery(document).ready(($) => {
 			scrollReveal($articles[i]);
 		}
 	})();
+
+	// header animation styling
+	// header text
+	const $heroTitle = $('.header__titleWrapper__title');
+	const $heroSubTitle = $('.header__titleWrapper__subtitle');
+	const tl = new TimelineMax()
+		.set($heroTitle, { x: -50 })
+		.set($heroTitle, { css: { opacity: '0' } })
+		.set($heroSubTitle, { x: -50 })
+		.set($heroSubTitle, { css: { opacity: '0' } })
+		.to($heroTitle, 1, { x: 0 })
+		.to($heroTitle, 2, { css: { opacity: '1' } }, '-=0.8')
+		.to($heroSubTitle, 1, { x: 0 }, '-=1')
+		.to($heroSubTitle, 2, { css: { opacity: '1' } }, '-=.8');
 });
